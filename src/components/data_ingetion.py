2b714e41a22d830438737bef1c017dd904599c00
Shashank Shukla <Shuklashashank297@gmail.com>
2023-07-24 17:36:10 +0530

    Added some code in data_ingetion

---
+++ src/components/data_ingetion.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.append(r'c:\Python_project\Customer-Personality-Analysis')
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
