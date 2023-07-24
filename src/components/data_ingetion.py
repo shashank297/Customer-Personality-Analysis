dd989abd8f1dd6103514266c68c90e38b7fb46c7
Shashank Shukla <Shuklashashank297@gmail.com>
2023-07-24 17:38:54 +0530

    Added some code in data_ingetion

--- src/components/data_ingetion.py
+++ src/components/data_ingetion.py
@@ -8,4 +8,15 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+@dataclass
+class DataIngetionconfig:
+    train_data_path:str=os.path.join('artifacts','train.csv')
+    test_data_path:str=os.path.join('artifact','test.csv')
+    raw_data_path:str=os.path.join('artifact','raw.csv')
+
+class DataIngetion:
+
+    def __init__(self):
+        self.ingestion_config=DataIngetionconfig()
+
 
