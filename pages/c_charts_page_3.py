c504c7add570d6ecc6ac637d512ac58bcb696ae2
Shashank Shukla <Shuklashashank297@gmail.com>
2023-08-21 16:44:44 +0530

    done

--- pages/c_charts_page_3.py
+++ pages/c_charts_page_3.py
@@ -110,10 +110,95 @@ def charts_page():
         fig.update_yaxes(title_text="Frequency", row=1, col=2)
         fig.update_yaxes(title_text="Frequency", row=2, col=1)
         fig.update_yaxes(title_text="Frequency", row=2, col=2)
+        fig.update_layout(showlegend=False)
+        fig.update_layout(height=800, width=800)
 
         # Create the Streamlit layout
         st.markdown("### Data Distributions")
         st.plotly_chart(fig, use_container_width=True)
+        
+        
+        # Create subplots with two columns and one row, specifying the subplot types
+        fig1 = sp.make_subplots(rows=1, cols=2, specs=[[{'type': 'pie'}, {'type': 'scatter'}]], subplot_titles=("Children Ratio", "Spending vs Income"))
+
+        # First Chart - Pie Chart
+        children_counts = df['children'].value_counts()
+        fig1.add_trace(go.Pie(
+            labels=children_counts.index,
+            values=children_counts.values,
+            marker=dict(colors=px.colors.sequential.RdBu),
+            textinfo='percent+label',
+            textfont_size=20,
+            marker_line=dict(color='white', width=2)
+        ), row=1, col=1)
+
+
+        # Second Chart - Scatter Plot
+        scatter_trace = px.scatter(df, x='income', y='spent', color='clusters', color_discrete_sequence=px.colors.qualitative.Set1).data[0]
+        fig1.add_trace(scatter_trace, row=1, col=2)
+
+        fig1.update_layout(showlegend=False)
+
+
+        # Update subplot layout for the second chart
+        fig1.update_xaxes(title_text="Income", row=1, col=2)
+        fig1.update_yaxes(title_text="Total Spendings", row=1, col=2)
+
+        # Create the Streamlit layout
+        st.markdown("### Children Ratio and Spending vs Income")
+        st.plotly_chart(fig1, use_container_width=True)
+
+        
+
+        cluster_purchases=df[['numdealspurchases','numwebpurchases','numcatalogpurchases','numstorepurchases','numwebvisitsmonth']].sum()
+        cam=df[['acceptedcmp1', 'acceptedcmp2', 'acceptedcmp3', 'acceptedcmp4','acceptedcmp5', 'response']].sum()
+        # Create subplots with 2 rows and 1 column
+        fig2 = sp.make_subplots(rows=1, cols=2,subplot_titles=("Segment-wise distribution on total purchases.", "campaign distribution"))
+
+        # Assuming cluster_purchases is your DataFrame with cluster-wise purchases
+        fig2.add_trace(
+            go.Bar(
+                x=cluster_purchases.index,
+                y=cluster_purchases.values,
+                marker_color=['#dc4c4c', '#157394', '#589cb4', '#bcb4ac', '#3c444c'],
+                text=cluster_purchases.values,
+                texttemplate='%{text:.2s}',
+                textposition='outside',
+                name='Cluster Purchases'
+            ),
+            row=1, col=1
+        )
+
+        # Customize the first subplot
+        fig2.update_xaxes(categoryorder='total ascending', tickangle=45, title_text='Clusters', title_font=dict(size=18), row=1, col=1)
+        fig2.update_yaxes(title_text='Purchases', title_font=dict(size=18), row=1, col=1)
+
+        # Assuming cam is your DataFrame with campaign-wise purchases
+        fig2.add_trace(
+            go.Bar(
+                x=cam.index,
+                y=cam.values,
+                marker_color=['#dc4c4c', '#157394', '#589cb4', '#bcb4ac', '#3c444c','#3c444c'],
+                text=cam.values,
+                texttemplate='%{text:.2s}',
+                textposition='outside',
+                showlegend=False
+            ),
+            row=1, col=2
+        )
+
+        # Customize the second subplot
+        fig2.update_xaxes(categoryorder='total ascending', tickangle=45, title_text='Campaigns', title_font=dict(size=18), row=1, col=2)
+        fig2.update_yaxes(title_text='No. of people', title_font=dict(size=18), row=1, col=2)
+
+        fig2.update_layout(showlegend=False)
+
+        # Set layout properties for the entire figure
+        fig2.update_layout(height=600, width=800)
+
+        st.markdown("### Segment-wise distribution on total purchases and campaign distribution.")
+        st.plotly_chart(fig2, use_container_width=True)
+
 
 
 
