e89bd1a89d6b2912dbcce789656995d7cc64125c
Shashank Shukla <shuklashashank297@gmail.com>
2023-09-10 20:24:49 +0530

    Update variable.py

--- src/components/variable.py
+++ src/components/variable.py
@@ -3,12 +3,12 @@ import psycopg2
 
 @dataclass
 class dataBase:
-    username='postgres'
-    password='.t7xdeH~SvJsPjn&'
-    hostname='34.131.10.52'
-    port='5432'
-    database_name='DB1'
-    db_url = f"postgresql://{username}:{password}@{hostname}:{port}/{database_name}"
+    username=''
+    password=''
+    hostname=''
+    port=''
+    database_name=''
+    db_url = f""
 
 
 
